10819eb99afae7962685baeebc71da66422d7677
harshidad <harshida.dd@gmail.com>
2025-07-04 11:59:26 -0700

    added folder for load url code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-import InputURL from './InputURL';
 import React, {useState} from 'react';
-import PagePreview from './PagePreview';
+import './App.css';
+
+import InputURL from './URLLoad/InputURL';
+import PagePreview from './URLLoad/PagePreview';
+
 function App() {
-    const [html, setHtml] = useState('');
-  const handleLoadPage=(url)=>{
+  const [html, setHtml] = useState('');
+  
+  const handleLoadURL=(url)=>{
       fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`)
      .then(res => res.json())
      .then(res=> setHtml(res.contents))
   }
-  // const handleLoadPage = async (url) => {
-  //   const res = await fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`);
-  //   const data = await res.json();
-  //   setHtml(data.contents);
-  // };
 
   return (
     <div className="App">
-      <InputURL onSubmit={handleLoadPage}></InputURL>
+      <InputURL onSubmit={handleLoadURL}></InputURL>
       <PagePreview html={html}></PagePreview>
     </div>
   );
--- src/URLLoad/InputURL.jsx
+++ src/URLLoad/InputURL.jsx
@@ -2,11 +2,11 @@ import React, {useState} from "react";
 
 const InputURL =({onSubmit})=>{
     const [url, setUrl] = useState('');
-
+    // value for URL: https://example.com
     return(
         <>
            <input type="text" value={url} onChange={(e)=> setUrl(e.target.value)}></input>
-           <button onClick={()=>onSubmit(url)}>Page Load</button>
+           <button onClick={()=> onSubmit(url)}>Page Load</button>
         </>
     );
 }
